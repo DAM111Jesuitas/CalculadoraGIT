4e977c8761a083537fcb69c6e8d8b7f7dd1aef21
DAM111 <dam11.jesuitas.2023@gmail.com>
2023-01-18 10:26:57 +0100

    Funcion restart

--- src/calculadoraGIT/Principal.java
+++ src/calculadoraGIT/Principal.java
@@ -2,6 +2,8 @@ package calculadoraGIT;
 
 public class Principal {
 	public static void main(String[] args) {
-		
+		Calculadora c = new Calculadora();
+		System.out.println(c.sumar(1, 3));
+		System.out.println(c.restar(1, 4));
 	}
 }
